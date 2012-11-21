e67291cc0a52474cdba8eb7137e2d4072514fd03
ChoJaewoo <chjw102@gmail.com>
2012-11-21 22:24:45 +0900

    deploy setting change user ubuntu

--- config/deploy.rb
+++ config/deploy.rb
@@ -7,64 +7,52 @@ set :repository, "git@github.com:ChoJaewoo/BatonTouch_Server.git"
 set :branch, "master"
 # Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
 
-role :web, "50.17.220.120"                          # Your HTTP server, Apache/etc
-role :app, "50.17.220.120"                          # This may be the same as your `Web` server
-role :db,  "50.17.220.120", :primary => true # This is where Rails migrations will run
+set :eip, "50.17.220.120"
 
-set :user, "deployer"
+role :web, eip                          # Your HTTP server, Apache/etc
+role :app, eip                          # This may be the same as your `Web` server
+role :db,  eip, :primary => true # This is where Rails migrations will run
+
+set :user, "ubuntu"
 set :deploy_to, "/home/#{user}/apps/#{application}"
 set :deploy_via, :remote_cache
 set :use_sudo, false
 
 default_run_options[:pty] = true
 ssh_options[:forward_agent] = true
+# ssh_options[:keys] = [File.join(ENV["HOME"], ".ssh", "chjw]
 
 after "deploy", "deploy:cleanup"
 
 namespace :deploy do
-	%w[start stop restart].each do |command|
-		desc "#{command} unicorn server"
-		task command, roles: :app, except: {no_release: true} do
-			run "/etc/init.d/unicorn_#{application} #{command}"
-		end
-	end
+  %w[start stop restart].each do |command|
+    desc "#{command} unicorn server"
+    task command, roles: :app, except: {no_release: true} do
+      run "/etc/init.d/unicorn_#{application} #{command}"
+    end
+  end
 
-	task :setup_config, roles: :app do
-		sudo "ln -nfs #{current_path}/config/nginx.conf /etc/nginx/sites-enabled/#{application}"
-		sudo "ln -nfs #{current_path}/config/unicorn_init.sh /etc/init.d/unicorn_#{application}"
-		run "mkdir -p #{shared_path}/config"
-		put File.read("config/database.example.yml"), "#{shared_path}/config/database.yml"
-		puts "Now edit the config files in #{shared_path}."
-	end
-	after "deploy:setup", "deploy:setup_config"
+  task :setup_config, roles: :app do
+    sudo "ln -nfs #{current_path}/config/nginx.conf /etc/nginx/sites-enabled/#{application}"
+    sudo "ln -nfs #{current_path}/config/unicorn_init.sh /etc/init.d/unicorn_#{application}"
+    run "mkdir -p #{shared_path}/config"
+    put File.read("config/database.example.yml"), "#{shared_path}/config/database.yml"
+    puts "Now edit the config files in #{shared_path}."
+  end
+  after "deploy:setup", "deploy:setup_config"
 
-	task :symlink_config, roles: :app do
-		run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml"
-	end
-	after "deploy:finalize_update", "deploy:symlink_config"
+  task :symlink_config, roles: :app do
+    run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml"
+  end
+  after "deploy:finalize_update", "deploy:symlink_config"
 
-	desc "Make sure local git is in sync with remote."
-	task :check_revision, roles: :web do
-		unless `git rev-parse HEAD` == `git rev-parse origin/master`
-			puts "WARNING: HEAD is not the same as origin/master"
+  desc "Make sure local git is in sync with remote."
+  task :check_revision, roles: :web do
+    unless `git rev-parse HEAD` == `git rev-parse origin/master`
+      puts "WARNING: HEAD is not the same as origin/master"
       puts "Run `git push` to sync changes."
       exit
-		end
-	end
-	before "deploy", "deploy:check_revision"
+    end
+  end
+  before "deploy", "deploy:check_revision"
 end
-
-# if you want to clean up old releases on each deploy uncomment this:
-# after "deploy:restart", "deploy:cleanup"
-
-# if you're still using the script/reaper helper you will need
-# these http://github.com/rails/irs_process_scripts
-
-# If you are using Passenger mod_rails uncomment this:
-# namespace :deploy do
-#   task :start do ; end
-#   task :stop do ; end
-#   task :restart, :roles => :app, :except => { :no_release => true } do
-#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
-#   end
-# end
